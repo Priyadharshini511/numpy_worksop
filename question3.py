886a4c2041100163505049f56cc20038ce909cd7
Priyadharshini511 <153971950+Priyadharshini511@users.noreply.github.com>
2024-05-20 14:26:01 +0530

    Update question3.py

--- question3.py
+++ question3.py
@@ -1,1 +1,10 @@
 #write a program to find the factorial of a nummber
+def fact(n):
+ if n == 0 or num == 1:
+  return 1
+ else:
+  return n * fact(n-1)
+num=(int(input))
+result=def(num)
+print(result)
+   
